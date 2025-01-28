40c5ac515a01c6e7b9debd0cc93302bc7c4ea4e3
27kike <88414138+27kike@users.noreply.github.com>
2025-01-28 11:55:40 -0600

    Página principal terminada
    Se ha terminado la página principal.
    Se agrego el purgecss.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
+const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
 //Función compila sass
 function css( done ){
@@ -8,7 +10,17 @@ function css( done ){
 		.pipe(dest('build/css'))//Guardar en ubicación
 	done();
 }
-
+function cssbuild( done ){
+	src('build/css/app.css')
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(purgecss({
+			content: ['*.html']
+		}))
+		.pipe(dest('build/css'));
+	done();
+}
 //Imagenes
 function imagenes( done ){
 	src('src/img/**/*')
@@ -26,3 +38,4 @@ exports.css = css;
 exports.imagenes = imagenes;
 exports.dev = dev; //gulp dev
 exports.default = series(imagenes, css, dev); //gulp
+exports.cssbuild = series(cssbuild); //gulp cssbuild
